9b3faa6a69632cd3da69b595ebedd512e8c2d316
YuraE1990 <70707157+YuraE1990@users.noreply.github.com>
2020-09-07 19:33:06 +0300

    Create 2.5.js

---
+++ day03/2.5.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <script>
+        let name = "Yura";
+
+        alert(`hello ${1}`); //display 'hello 1'
+
+        alert(`hello ${"name"}`); //display 'hello name'
+
+        alert(`hello ${name}`); //display 'hello Serj'
+    </script>
+</body>
+
+</html>
