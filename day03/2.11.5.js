b61fa1655d86ab7867d9e854f840b14f071ddbb0
YuraE1990 <70707157+YuraE1990@users.noreply.github.com>
2020-09-20 18:11:43 +0300

    Create 2.11.5.js

---
+++ day03/2.11.5.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <script>
+
+alert( null || 2 && 3 || 4 );
+//сначала преобразует в вид null||3||4, а затем вернет 3 как первое истинное
+
+</script>
+</body>
+</html>
