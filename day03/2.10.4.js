bc460adccd3aa6cfdaa37298094bd3d23d8adce0
YuraE1990 <70707157+YuraE1990@users.noreply.github.com>
2020-09-20 18:11:58 +0300

    Create 2.10.4.js

---
+++ day03/2.10.4.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>learnJS</title>
+  </head>
+  <body>
+    <script>
+      let message =
+        login == "Сотрудник"
+          ? "Привет"
+          : login == "Директор"
+          ? "Здравствуйте"
+          : login == ""
+          ? "Нет логина"
+          : "";
+    </script>
+  </body>
+</html>
